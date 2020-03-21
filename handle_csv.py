da8fdf3e3dc7c4d822586a542d5944358ebf2121
HenryDLC <HenryDLC@users.noreply.github.com>
2020-03-21 15:34:52 +0800

    Update handle_csv.py

--- handle_csv.py
+++ handle_csv.py
@@ -54,18 +54,15 @@ def top_ten_turn_volume(sub_li):
 
 
 def request_to_list(r_flatten_list, group_step):
-
     r_stock_list = []
     # 分组处理, 步长group_step
     for i in range(0, len(r_flatten_list), group_step):
-        print("request index=%d" % i)
         sub_list = r_flatten_list[i:i + group_step]
         sub_flatten_str = ",".join(sub_list)
         r = requests.get("http://hq.sinajs.cn/list=" + sub_flatten_str)
         if r.status_code == 200:
             # print "=======", str(i), "success========"
             split_lines_list = r.text.splitlines()
-
             for line in split_lines_list:
                 stock_id = line[line.find("=") - 8:line.find("=")]
                 # 格式: var hq_str_sh201004="R091,0.000,4.450,4.450,2019-02-20,11:30:00,03";
@@ -80,41 +77,42 @@ def request_to_list(r_flatten_list, group_step):
 
     return r_stock_list
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     TEMPLATE = "template.html"
     INDEX_HTML = "index.html"
     CSV_PATH = "stock.csv"
+    while 1:
+        with open(CSV_PATH, 'r') as f:
+            reader = csv.reader(f)
+            your_list = list(reader)
 
-    with open(CSV_PATH, 'r') as f:
-        reader = csv.reader(f)
-        your_list = list(reader)
+            # list扁平化 [['a'],['b'],['c']] ==> ['a','b','c']
+            flatten_list = np.array(your_list).flatten().tolist()
+            del flatten_list[0]
 
-        # list扁平化 [['a'],['b'],['c']] ==> ['a','b','c']
-        flatten_list = np.array(your_list).flatten().tolist()
-        del flatten_list[0]
+            stock_list = request_to_list(flatten_list, 50)
 
-        stock_list = request_to_list(flatten_list, 50)
+            price_list = top_ten_by_price(stock_list)
+            range_list = top_ten_by_range(stock_list)
+            range_r_list = top_ten_by_range_r(stock_list)
+            volume_list = top_ten_by_volume(stock_list)
+            turn_volume_list = top_ten_turn_volume(stock_list)
 
-        price_list = top_ten_by_price(stock_list)
-        range_list = top_ten_by_range(stock_list)
-        range_r_list = top_ten_by_range_r(stock_list)
-        volume_list = top_ten_by_volume(stock_list)
-        turn_volume_list = top_ten_turn_volume(stock_list)
+            context = dict()
+            context["price_list"] = price_list
+            context["range_list"] = range_list
+            context["range_r_list"] = range_r_list
+            context["volume_list"] = volume_list
+            context["turn_volume_list"] = turn_volume_list
+            context["stock_list"] = stock_list
 
-        context = dict()
-        context["price_list"] = price_list
-        context["range_list"] = range_list
-        context["range_r_list"] = range_r_list
-        context["volume_list"] = volume_list
-        context["turn_volume_list"] = turn_volume_list
-        context["stock_list"] = stock_list
+            html = template(TEMPLATE, items=context)
 
-        html = template(TEMPLATE, items=context)
-
-        with open(INDEX_HTML, 'wb') as f:
-            f.write(html.encode('utf-8'))
-            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-            print("=======success========")
-            f.close()
-
+            with open(INDEX_HTML, 'wt') as f:
+                f.write(html.encode('utf-8'))
+                print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                print "=======success========"
+                f.close()
+            import time
+            time.sleep(10)
